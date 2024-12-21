ba52276514700fe37efe427fa7b38448b327b779
gyeongmann <hkc8857@naver.com>
2024-12-22 00:45:37 +0900

    create chat channel

--- Chat/src/components/UserListItem.tsx
+++ Chat/src/components/UserListItem.tsx
@@ -1,36 +1,51 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Pressable } from 'react-native';
 import React from 'react';
 import { supabase } from '../lib/superbase';
+import { useChatContext } from 'stream-chat-expo';
+import { useAuth } from '../providers/AuthProvider';
+import { router } from 'expo-router';
 
 const UserListItem = ({ user }: any) => {
-    console.log('userListime:', user);
+    const { client } = useChatContext();
+    const { user: me } = useAuth();
+
+    const onPress = async () => {
+        const channel = client.channel('messaging', {
+            members: [me?.id, user.id],
+        });
+        await channel.watch();
+        router.replace(`/(home)/channel/${channel.cid}`);
+    };
+    // console.log('userListime:', user);
 
     const publicUrl = supabase.storage
         .from('avatars')
         .getPublicUrl(user.avatar_url)?.data?.publicUrl;
 
     return (
-        <View
-            style={{
-                padding: 15,
-                backgroundColor: 'white',
-                flexDirection: 'row',
-                alignItems: 'center',
-                marginTop: 1,
-            }}
-        >
-            <Image
-                source={{ uri: publicUrl }}
+        <Pressable onPress={onPress}>
+            <View
                 style={{
-                    width: 50, // 원하는 크기로 설정
-                    height: 50,
-                    borderRadius: 25, // 원형으로 만들기
-                    marginBottom: 10,
-                    marginRight: 20,
+                    padding: 15,
+                    backgroundColor: 'white',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginTop: 1,
                 }}
-            />
-            <Text style={{ fontWeight: '600' }}>{user.username}</Text>
-        </View>
+            >
+                <Image
+                    source={{ uri: publicUrl }}
+                    style={{
+                        width: 50, // 원하는 크기로 설정
+                        height: 50,
+                        borderRadius: 25, // 원형으로 만들기
+                        marginBottom: 10,
+                        marginRight: 20,
+                    }}
+                />
+                <Text style={{ fontWeight: '600' }}>{user.username}</Text>
+            </View>
+        </Pressable>
     );
 };
 
