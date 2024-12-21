1e0ebde9b7a342aac2ce1908eacf0d44ba19fcbe
gyeongmann <hkc8857@naver.com>
2024-12-21 23:08:49 +0900

    profile image

--- Chat/src/app/(home)/(tabs)/profile.tsx
+++ Chat/src/app/(home)/(tabs)/profile.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import { supabase } from '../../../lib/superbase';
-import { StyleSheet, View, Alert } from 'react-native';
+import { StyleSheet, View, Alert, ScrollView } from 'react-native';
 import { Button, Input } from '@rneui/themed';
 import { useAuth } from '@/src/providers/AuthProvider';
+import Avatar from '@/src/components/Avartar';
 
 export default function ProfileScreen() {
     const { session } = useAuth();
@@ -84,7 +85,22 @@ export default function ProfileScreen() {
     }
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
+            <View style={{ alignItems: 'center' }}>
+                <Avatar
+                    size={200}
+                    url={avatarUrl}
+                    onUpload={(url: string) => {
+                        setAvatarUrl(url);
+                        updateProfile({
+                            username,
+                            website,
+                            avatar_url: url,
+                            full_name: fullName,
+                        });
+                    }}
+                />
+            </View>
             <View style={[styles.verticallySpaced, styles.mt20]}>
                 <Input label="Email" value={session?.user?.email} disabled />
             </View>
@@ -131,7 +147,7 @@ export default function ProfileScreen() {
                     onPress={() => supabase.auth.signOut()}
                 />
             </View>
-        </View>
+        </ScrollView>
     );
 }
 
